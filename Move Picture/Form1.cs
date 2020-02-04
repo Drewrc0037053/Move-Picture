590a63a61789b712436d4c3c9572f3e6cb476233
Drew Doran <rc0037053@student.riversidecollege.ac.uk>
2020-02-04 11:44:03 +0000

    Task 2.2.b + Task 2.2.c I added a third and fourth button to make the picture be able to move both upwards and downwards

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -37,5 +37,15 @@ namespace Move_Picture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 20, pictureBox1.Location.Y);
         }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 20);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 20);
+        }
     }
 }
